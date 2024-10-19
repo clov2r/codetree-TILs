e70105df5b64864e38cedf2ff032bbbd717ab0ef
CHOI MYEONGJI <118502976+clov2r@users.noreply.github.com>
2024-10-19 23:12:08 +0900

    Title: 최대로 겹치는 구간, Time: 130ms, Memory: 25MB, Status: Passed - CodeTree

--- 241019/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 241019/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -1,7 +1,11 @@
-n=int(input())
-x_list=[0]*100
+n = int(input())
+x_list = [0] * 201  # -100에서 100까지 다루기 위해 리스트 크기를 201로 설정
+
 for i in range(n):
-    x, y=map(int, input().split())
-    for j in range(x-1, y):
-        x_list[j]+=1
+    x, y = map(int, input().split())
+    # 선분의 끝점 x2는 포함하지 않으므로 x부터 y-1까지만 처리
+    for j in range(x + 100, y + 100):  # 인덱스를 100 더해 양수로 변환
+        x_list[j] += 1
+
+# 가장 많이 겹치는 구간에서의 겹치는 선분 수 출력
 print(max(x_list))
